d144c199d39c12d13c666b307cbb0a17a5bbd0d8
LizaG000 <elizaveta.vladimirovna.g@gmail.com>
2025-07-28 11:53:47 +0300

    настроено api и бд

--- src/main/web.py
+++ src/main/web.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from src.main.config import config
+from src.presentation.fastapi.setup import setup_routes
 
 app = FastAPI(
-    title=config.api.project_name,
-    redoc_url=None,
-    openapi_url=None,
-    docs_url=None,
+    title=config.api.project_name
 )
+
+setup_routes(app, config)
